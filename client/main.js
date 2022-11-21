014c9f63540b3be486f89bac09ba629549c9bf88
Jacob Marillion <jacobmarillion@gmail.com>
2022-11-20 18:37:29 -0700

    Added Form

--- client/main.js
+++ client/main.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+//Need app . get code here for Cors and Express.
+
+//Add Committment Button and Form Code.
+//TODO does not work.
+document.getElementById("addButton").addEventListener("click", function() {
+    document.querySelector(".popModal").mainStyles.display = "flex"
+});
+
+document.querySelector(".closeButton").addEventListener("click", function() {
+    document.querySelector(".popModal").mainStyles.display = "none"
+});
